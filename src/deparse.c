cf897726c4213bdb5dc932cee98dfb7126888783
alrocar <alrocar@tinybird.co>
2024-10-02 11:05:18 +0200

    support extract

--- src/deparse.c
+++ src/deparse.c
@@ -2468,6 +2468,50 @@ deparseFuncExpr(FuncExpr *node, deparse_expr_cxt *context)
 		appendStringInfoChar(buf, ')');
 		return;
 	}
+	else if (cdef && cdef->cf_type == CF_EXTRACT)
+	{
+		if (list_length(node->args) != 2)
+        	elog(ERROR, "extract requires exactly two arguments");
+
+		Const *arg = (Const *) linitial(node->args);
+		char *parttype = TextDatumGetCString(arg->constvalue);
+		CSTRING_TOLOWER(parttype);
+
+		Expr *source_expr = (Expr *) lsecond(node->args);
+
+		if (strcmp(parttype, "day") == 0)
+			appendStringInfoString(buf, "toDayOfMonth");
+		else if (strcmp(parttype, "doy") == 0)
+			appendStringInfoString(buf, "toDayOfYear");
+		else if (strcmp(parttype, "dow") == 0)
+			appendStringInfoString(buf, "toDayOfWeek");
+		else if (strcmp(parttype, "year") == 0)
+			appendStringInfoString(buf, "toYear");
+		else if (strcmp(parttype, "month") == 0)
+			appendStringInfoString(buf, "toMonth");
+		else if (strcmp(parttype, "hour") == 0)
+			appendStringInfoString(buf, "toHour");
+		else if (strcmp(parttype, "minute") == 0)
+			appendStringInfoString(buf, "toMinute");
+		else if (strcmp(parttype, "second") == 0)
+			appendStringInfoString(buf, "toSecond");
+		else if (strcmp(parttype, "quarter") == 0)
+			appendStringInfoString(buf, "toQuarter");
+		else if (strcmp(parttype, "isoyear") == 0)
+			appendStringInfoString(buf, "toISOYear");
+		else if (strcmp(parttype, "week") == 0)
+			appendStringInfoString(buf, "toISOWeek");
+		else if (strcmp(parttype, "epoch") == 0)
+			appendStringInfoString(buf, "toUnixTimestamp");
+		else
+			elog(ERROR, "extract cannot be exported for: %s", parttype);
+
+		pfree(parttype);
+		appendStringInfoChar(buf, '(');
+		deparseExpr(source_expr, context);
+		appendStringInfoChar(buf, ')');
+		return;
+	}
 	else if (cdef && cdef->cf_type == CF_ISTORE_SEED)
 	{
 		if (!context->func)
